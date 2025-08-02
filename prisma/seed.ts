7a6e14e3e1c96e321dc63685da64d35d779074ef
Nuray <nuraynurzhaskyzy@gmail.com>
2025-08-02 21:27:37 +0500

    Adding prisma with SQLite

---
+++ prisma/seed.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const password = await bcrypt.hash('test1234', 10);
+  await prisma.user.upsert({
+    where: { email: 'test@example.com' },
+    update: {},
+    create: {
+      email: 'test@example.com',
+      password,
+    },
+  });
+}
+
+main().finally(() => prisma.$disconnect());
