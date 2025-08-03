5d48ccb983320ab9dac74e7b36b183dc4c0224c5
Nuray <nuraynurzhaskyzy@gmail.com>
2025-08-03 10:22:10 +0500

    Adding CORS

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
