7ee568dd28245e4d5e65424671f2114665382824
Nuray <nuraynurzhaskyzy@gmail.com>
2025-08-03 09:54:12 +0500

    Changing port

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
