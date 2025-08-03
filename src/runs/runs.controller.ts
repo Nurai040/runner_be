e6beee7aa6c99e74c39ab42f3bd2b5e86e8f8f1e
Nuray <nuraynurzhaskyzy@gmail.com>
2025-08-03 10:43:33 +0500

    Addin photo

--- src/runs/runs.controller.ts
+++ src/runs/runs.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Post, Get, Body, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Req,
+  UseGuards,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { RunsService } from './runs.service';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('runs')
 @UseGuards(AuthGuard('jwt'))
@@ -9,10 +19,18 @@ export class RunsController {
   constructor(private readonly runsService: RunsService) {}
 
   @Post()
-  createRun(@Body() body, @Req() req: Request) {
-    return this.runsService.createRun(body, (req as any).user.userId);
-  }
+  @UseGuards(AuthGuard('jwt'))
+  @UseInterceptors(FileInterceptor('photo'))
+  createRun(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() body,
+    @Req() req,
+  ) {
+    const userId = req.user.userId;
+    const photoUrl = file ? `/uploads/${file.filename}` : null;
 
+    return this.runsService.createRun({ ...body, photoUrl }, userId);
+  }
   @Get()
   getRuns(@Req() req: Request) {
     const user = (req as any).user.userId;
