e076fcfec6931c192779873107c918b0a39f3017
Nuray <nuraynurzhaskyzy@gmail.com>
2025-08-03 11:27:41 +0500

    Fixing upload

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { extname } from 'path';
+import { extname, join } from 'path';
 
 @Controller('upload')
 export class UploadController {
@@ -14,7 +14,7 @@ export class UploadController {
   @UseInterceptors(
     FileInterceptor('file', {
       storage: diskStorage({
-        destination: './uploads',
+        destination: join(__dirname, '..', '..', 'uploads'), // ✅ updated
         filename: (req, file, cb) => {
           const uniqueSuffix =
             Date.now() + '-' + Math.round(Math.random() * 1e9);
